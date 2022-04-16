e57f9b33746411efb5d3d1710a224c89cf58b757
404Xavier <wakirithomaitho100@protonmai.com>
2022-04-17 00:15:58 +0300

    Done: day 56 - using styling and css classes

--- 56.DAY/main.js
+++ 56.DAY/main.js
@@ -18,3 +18,43 @@ document.body.style.display = 'none';
 setTimeout(() => {
 	document.body.style.display = '';
 }, 2000);
+
+//store the class
+const para = document.querySelector('#para');
+console.log(para);
+//set stylles using element .style
+para.style.textAlign = 'center';
+//addding styles to to the style
+para.setAttribute(
+	'style',
+	'color: dodgerblue; textAlign: center; fontSize:  24px'
+);
+para.style.cssText =
+	'color: dodgerblue; fontSize:  24rem; backgroundColor: pink';
+//using style.cssText
+console.dir(para);
+
+const headingComp = getComputedStyle(heading);
+console.log(headingComp); //{...}
+
+//select the main
+const main = document.querySelector('.main');
+console.log(main);
+//select the div
+const div = main.querySelector('div');
+//creeate the function to show notifications
+const showNotication = ({ top, right, message, className }) => {
+	div.style.cssText = `top: ${top}px; right: ${right}px;`;
+	div.classList.add(className);
+	div.classList.add('green');
+	div.innerText = message;
+};
+
+const items = {
+	top: 10,
+	right: 10,
+	message: 'Hello there, welcome!!!',
+	className: 'welcome'
+};
+
+showNotication(items);
