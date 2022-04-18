26b778c4c11477be675607badc01d7e54ba900b1
404Xavier <wakirithomaitho100@protonmai.com>
2022-04-18 21:59:19 +0300

    Day 57 done: Scrolling window

--- 57.DAY/main.js
+++ 57.DAY/main.js
@@ -4,58 +4,68 @@
  * @format
  */
 
-const divScroll = document.querySelector('#text');
-console.log(divScroll.offsetLeft); //32
-console.log(divScroll.offsetTop); //32
+// const divScroll = document.querySelector('#text');
+// console.log(divScroll.offsetLeft); //32
+// console.log(divScroll.offsetTop); //32
 
-console.dir(divScroll.offsetWidth); //390
-console.dir(divScroll.offsetHeight); //290
+// console.dir(divScroll.offsetWidth); //390
+// console.dir(divScroll.offsetHeight); //290
 
-//clientLeft and clientTop - the width of the border
-console.log(divScroll.clientTop); //25
-console.log(divScroll.clientLeft); //25
-console.log(divScroll.clientRight); //undefined
-console.log(divScroll.clientBottom); //undefined
+// //clientLeft and clientTop - the width of the border
+// console.log(divScroll.clientTop); //25
+// console.log(divScroll.clientLeft); //25
+// console.log(divScroll.clientRight); //undefined
+// console.log(divScroll.clientBottom); //undefined
 
-/***
- * clientHeight and clientWidth
- * They represent inside element borders content together with padding
- */
+// /***
+//  * clientHeight and clientWidth
+//  * They represent inside element borders content together with padding
+//  */
 
-console.log(divScroll.clientWidth); //325 (300 + 25)
-console.log(divScroll.clientHeight); //225 (200+25)
+// console.log(divScroll.clientWidth); //325 (300 + 25)
+// console.log(divScroll.clientHeight); //225 (200+25)
 
-/***
- * scrollHeight and scrollWidth
- */
-console.log(divScroll.scrollWidth); //325
-console.log(divScroll.scrollHeight); //346
+// /***
+//  * scrollHeight and scrollWidth
+//  */
+// console.log(divScroll.scrollWidth); //325
+// console.log(divScroll.scrollHeight); //346
 
-/***
- * scrollLeft and scrollHeight
- */
+// /***
+//  * scrollLeft and scrollHeight
+//  */
 
-console.log(divScroll.scrollTop); //0 if the items insde are not scrolled
-console.log(divScroll.scrollTop); //0
+// console.log(divScroll.scrollTop); //0 if the items insde are not scrolled
+// console.log(divScroll.scrollTop); //0
 
-/***
- * Window and scrolling
- * document.documentElement is the window width and height
- */
+// /***
+//  * Window and scrolling
+//  * document.documentElement is the window width and height
+//  */
 
-const docEl = document.documentElement;
-console.log(docEl.clientWidth); //1366 if there is a scroobar the clientWdith is 1351 15 is off the scrollbar
-console.log(window.innerWidth); //1366 -> doesn't account the width of the scrollbar
+// const docEl = document.documentElement;
+// console.log(docEl.clientWidth); //1366 if there is a scroobar the clientWdith is 1351 15 is off the scrollbar
+// console.log(window.innerWidth); //1366 -> doesn't account the width of the scrollbar
 
-/***
- * Getting the current scroll
- *
- */
-console.log(docEl.scrollTop); //0
-console.log( docEl.scrollLeft ); //0
+// /***
+//  * Getting the current scroll
+//  *
+//  */
+// console.log(docEl.scrollTop); //0
+// console.log(docEl.scrollLeft); //0
 
+// document.addEventListener('scroll', () => {
+// 	//commented oot for performance reasons
+// 	// console.log(window.pageXOffset);
+// 	// console.log(window.pageYOffset);
+// });
+
+document.addEventListener('click', () => {
+	// window.scrollBy(0, 30);//scrolls the element by 30 for every click
+	// window.scrollTo(0, 50);
+});
+
+//listen to the scroll event
 document.addEventListener( 'scroll', () => {
-	//commented oot for performance reasons
-	// console.log(window.pageXOffset);
-	// console.log(window.pageYOffset);
-})
+	//
+});
