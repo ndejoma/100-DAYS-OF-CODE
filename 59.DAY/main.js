14280222a7521962ed2d60c6b94e70b747d7be07
404Xavier <wakirithomaitho100@protonmai.com>
2022-04-19 21:51:54 +0300

    Events objects start

---
+++ 59.DAY/main.js
@@ -0,0 +1,21 @@
+/**
+ * The event object
+ *
+ * @format
+ */
+
+const btn = document.querySelector('#btn');
+console.log(btn);
+
+//event handler
+const handleClick = function (event) {
+	console.log(event.currentTarget); //<button id="btn">Click Me!!! H</button>
+	console.log(this); //<button id="btn">Click Me!!! H</button>
+	console.log(event); //PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, ...}e
+	//log the coordinates of the mursor where the mouse clicked
+	console.log(`The mouse clicked X:${event.clientX}, Y:${event.clientY}`);//The mouse clicked X:68, Y:25
+	console.log(`The type of the event is ${event.type}`);//The type of the event is click
+};
+
+//add the eventlistenr and event hanlder
+btn.addEventListener('click', handleClick);
