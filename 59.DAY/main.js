420ea60d92ff803ee17873bb4795c46f8dbb8fd8
404Xavier <wakirithomaitho100@protonmai.com>
2022-04-20 22:17:52 +0300

    Day 59: Random color generator

--- 59.DAY/main.js
+++ 59.DAY/main.js
@@ -4,22 +4,37 @@
  * @format
  */
 
-const btn = document.querySelector('#btn');
-const divText = document.querySelector('#text');
-console.log(btn);
+//select the button with a class of
+const btn = document.querySelector('.btn');
+const colorName = document.querySelector('.color');
 
-// //event handler
-// const handleClick = function (event) {
-// 	console.log(event.currentTarget); //<button id="btn">Click Me!!! H</button>
-// 	console.log(this); //<button id="btn">Click Me!!! H</button>
-// 	console.log(event); //PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, ...}e
-// 	//log the coordinates of the mursor where the mouse clicked
-// 	console.log(`The mouse clicked X:${event.clientX}, Y:${event.clientY}`);//The mouse clicked X:68, Y:25
-// 	console.log(`The type of the event is ${event.type}`);//The type of the event is click
-// };
+//create a function to generate random color in rgb(r, g, b) between 0 and 255
+const randomNo = max => {
+	return Math.floor(Math.random() * max);
+};
 
-//add the eventlistenr and event hanlder
-btn.addEventListener('click', e => {
-	//hide or show the div#text
-	divText.classList.toggle('hidden');
+//rest random generation between 0 and 1o that is 0-9
+// console.log(randomNo(10));
+
+//create a function to get randomColor
+const getRandomColor = () => {
+	const r = randomNo(255);
+	const g = randomNo(255);
+	const b = randomNo(255);
+	//return rgb format
+	return `rgb(${r},${g},${b})`;
+};
+
+//create a function to render the color
+const renderColor = (targetElement = document.body, colorNameCont) => {
+	const color = getRandomColor();
+	//set the body to the generated random color
+	targetElement.style.backgroundColor = color;
+	//set the name to be the current color
+	colorNameCont.innerText = color;
+};
+
+//generate the random Color on the click of the button
+btn.addEventListener('click', () => {
+	renderColor(document.body, colorName);
 });
