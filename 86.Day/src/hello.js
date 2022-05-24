154f5d3784df5a32e1f0b092f06beeb6afab3f17
404Xavier <wakirithomaitho100@protonmai.com>
2022-05-24 11:56:18 +0300

    Day 86 to 89 done

--- 86.Day/src/hello.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-const nameMe = 'John Maina';
-const message = 'Hello there';
-
-console.log('***I get called from the hello.js file****');
-console.log(message, nameMe);
-console.log('***I get called from the hello.js file****');
-console.log('I am awesome');
