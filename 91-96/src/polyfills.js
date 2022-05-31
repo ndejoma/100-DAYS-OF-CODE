5ad8d8c2ff649ea07a9cf91add4b2f1ff080a773
404Xavier <wakirithomaitho100@protonmai.com>
2022-06-01 01:13:48 +0300

    Correct the result of the groupBy method

--- 91-96/src/polyfills.js
+++ 91-96/src/polyfills.js
@@ -7,7 +7,7 @@ setTimeout(() => {
 }, 0);
 
 console.log(Array.from(new Set([1, 2, 3, 2, 1]))); // => [1, 2, 3]
-console.log([1, 2, 3, 4, 5].groupBy(it => it % 2)); // => { 1: [1, 3, 5], 0: [2, 4] }
+console.log([1, 2, 3, 4, 5].groupBy(it => it % 2)); // => { '0': [ 2, 4 ], '1': [ 1, 3, 5 ] }
 console.log(Promise.resolve(42).then(x => console.log(x))); // => 42
 structuredClone(new Set([1, 2, 3])); // => new Set([1, 2, 3])
 queueMicrotask(() => console.log('called as microtask'));
